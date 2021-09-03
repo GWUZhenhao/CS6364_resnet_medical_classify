2209ec1d5ed0b9a4b5946c203fb9e99f5c18134a
GWUZhenhao <74644112+GWUZhenhao@users.noreply.github.com>
2021-09-02 20:02:16 -0400

    explore the dataset (working).

--- Dataset_explorer.py
+++ Dataset_explorer.py
@@ -15,6 +15,21 @@ df_master_2 = pd.read_csv(path_df_master_2, keep_default_na=False)
 types_1 = [i for i in df_master_1['Category'] if i != '']
 types_2 = [i for i in df_master_2['Category'] if i != '']
 
-print(types_1 == types_2)
-print(len(types_1))
-print(len(types_2))
+# Check 2 sub-dataset is equal or not
+print('two dataframe is equal or not: {}'.format(types_1 == types_2))
+print('The length of first dataframe is: {}.'.format(len(types_1)))
+
+# Explore the types of the cell
+types = [i for i in np.unique(types_1) if ',' not in i]
+print('There are {} types of cells'.format(len(types)))
+print('The types of dataset is {}'.format(types))
+df_Neu = df_master_1[df_master_1['Category'] == 'NEUTROPHIL']
+df_EOS = df_master_1[df_master_1['Category'] == 'EOSINOPHIL']
+df_Mon = df_master_1[df_master_1['Category'] == 'MONOCYTE']
+df_Bas = df_master_1[df_master_1['Category'] == 'BASOPHIL']
+df_Lym = df_master_1[df_master_1['Category'] == 'LYMPHOCYTE']
+print('The number of Eosinophil is {}'.format(df_EOS))
+print('The number of Neutrophil is {}'.format(df_Neu))
+print('The number of Basophil is {}'.format(df_Bas))
+print('The number of Monocyte is {}'.format(df_Mon))
+print('The number of Lymphocyte is {}'.format(df_Lym))
